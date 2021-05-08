7b4018e4338c69fc0cdee2b85a1135b99602d97f
iadirock <aditya16219@iiitd.ac.in>
2021-05-08 11:52:30 +0530

    Show the blogs from db to home page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def allblogs(request):
+    #to get objects from db and turn them into python objects
+    blogs = Blog.objects
+    return render(request, 'blogs/allblogs.html', {'blogs':blogs})
